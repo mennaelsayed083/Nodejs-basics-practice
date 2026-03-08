4b514f321d522db2bbfc079f3da5bed05c6f444d
Your Namemenna <menna@gmail.com>
2026-03-08 16:21:00 +0200

    second commit

--- event.js
+++ event.js
@@ -7,18 +7,17 @@ event.on("sum",(x,y)=>{
 })
 
 event.emit("sum",5,6)
-////////////////////////////////////////////////////////////
+
 event.once("email",(emails)=>{
     console.log(`send your questions on this emails:     ${emails}`)
 })
-event.emit("email","tennnnnnnnnnnnnnnnnna@com")
+event.emit("email","test@gmail.com")
 event.emit("email"," menna@com")
 
 event.emit("email","adam@com")
 
-/////////////////////////////////////////////////////////////////////
 function logUse() {
-    console.log("yaaaaaaaaaaaaaaaaaaaaa")
+    console.log(" ......... ")
     
 }
 event.on("user",logUse)
@@ -26,10 +25,10 @@ event.removeListener("user",logUse)
 event.emit("user")
 
 
-console.log(event.eventNames())    // once delete event after print once only and remove deleted by yourself
+console.log(event.eventNames())    
 
 
 event.on("end",()=>{
-    console.log("enddddddddddddddddddddddddddddddd")
+    console.log("enddddd")
 })
    
